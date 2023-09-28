6423403b8d494cdbfe2cdf3125ad984bae8d9e28
DouweKapteijn <douwiaan@gmail.com>
2023-09-28 10:45:36 +0200

    hero improved readability

--- src/components/home/homehero.js
+++ src/components/home/homehero.js
@@ -7,10 +7,10 @@ function Homehero() {
 
         <section className="bg-transparent">
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img src={Devpic} alt="mockup" />
+                <div className="order-2 lg:order-1 lg:mt-0 lg:col-span-5 flex">
+                    <img className='mx-auto' src={Devpic} alt="mockup" />
                 </div>  
-                <div className="mx-auto place-self-center lg:col-span-7">
+                <div className="order-1 lg:order-2 mx-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl text-ultradarkpurple">
                         {/* Douwe Kapteijn. Development zonder limiet. */}
                         <Typewriter
@@ -25,11 +25,11 @@ function Homehero() {
                             }}
                         />
                     </h1>
-                    <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">
+                    <p className="max-w-2xl mb-6 font-[350] lg:mb-8 md:text-lg lg:text-xl text-gray-600">
                         Douwe Kapteijn is een jonge developer uit Amersfoort die graag aan het werk gaat om nieuwe manieren van development te leren.
-                        Hij Studeert momenteel software development op het Deltion college in Zwolle op MBO niveau 4.
+                        Ik studeer momenteel software development op het Deltion college in Zwolle op MBO niveau 4.
                         <br />
-                        Hij houd zich vooral bezig met frontend development, maar is ook niet vies van een beetje backend.
+                        Ik hou mij vooral bezig met frontend development, maar ik ben ook niet vies van een beetje backend.
                     </p>
                 </div>              
             </div>
